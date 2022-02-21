701c95bf13c90475c22847a473aaac9f410cb4cb
carloscrusca <42319133+carloscrusca@users.noreply.github.com>
2022-02-21 10:07:52 -0300

    Impressão Cep na tela

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-alert("Javascript rodou")
+function consultaCep(){
+    var cep = document.getElementById("cep").value;
+    var url = "https://viacep.com.br/ws/" + cep + "/json/";
+    console.log(cep);
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function(response){
+            console.log(response);
+            $("#logradouro").html(response.logradouro);
+            //document.getElementById("logradouro").innerHTML = response.logradouro;
+            document.getElementById("localidade").innerHTML = response.localidade;
+        }
+    } )
+}
